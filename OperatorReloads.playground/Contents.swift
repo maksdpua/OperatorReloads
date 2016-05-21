01b5c2829956eb671df59a21c4a95564cebe1631
Maks <maksdpua@gmail.com>
2016-05-21 15:07:30 +0300

    Final

--- OperatorReloads.playground/Contents.swift
+++ OperatorReloads.playground/Contents.swift
@@ -91,7 +91,53 @@ func %%% (inout a: Point, b: Point) -> Point {
 
 p1 %%% p2
 
+var j = 5
+
+i
+j
 
 
+i / 6 / j
+
+infix operator ** {associativity right precedence 110}
+
+func ** (a: Point, b: Point) -> Point {
+    return Point(x: 2 * (a.x + b.x), y: 2 * (a.y + b.y))
+}
+
+p1 = Point(x: 2, y: 3)
+p2 = Point(x: 3, y: 5)
+
+p1 ** p2 ** p1 ** p2
+
+p1 + p2 ** p2 ** p1
+((p1 + p2) ** p2) ** p1
+(p1 + p2) ** (p2 ** p1)
+
+
+var s = "Hello, world!"
+
+
+import Foundation
+
+func -= (inout s1: String, s2: String) {
+    let set = NSCharacterSet(charactersInString: s2)
+    let components : NSArray = s1.componentsSeparatedByCharactersInSet(set)
+    s1 = components.componentsJoinedByString("")
+}
+
+s -= "lo"
+
+
+
+func -= (inout s1: String, i: Int) {
+    s1 -= String(i)
+}
+
+s = "0123456789"
+
+s -= 5784440
+
+s
 
 
